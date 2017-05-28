92e5e4028840ec75bb0d297c92b9f8f8ed81f9e3
happylrd <happylrd17@gmail.com>
2017-05-28 14:32:22 +0800

    Init project structure
    and add the basic Header component with ant design

---
+++ ynews/src/components/PCIndex.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import PCHeader from './PCHeader';
+
+export default class PCIndex extends Component {
+    render() {
+        return (
+            <div>
+                <PCHeader/>
+            </div>
+        );
+    }
+}
