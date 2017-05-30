75ed7c6b394d49800273d43d87f99ddd9e91f8ea
happylrd <happylrd17@gmail.com>
2017-05-30 16:38:09 +0800

    Add tabs for mobile

--- ynews/src/components/MobileIndex.js
+++ ynews/src/components/MobileIndex.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
+import {Tabs} from 'antd';
 
 import MobileHeader from './MobileHeader';
 import MobileFooter from './MobileFooter';
+
+const TabPane = Tabs.TabPane;
 
 export default class MobileIndex extends Component {
 
@@ -9,6 +12,23 @@ export default class MobileIndex extends Component {
         return (
             <div>
                 <MobileHeader/>
+                <Tabs>
+                    <TabPane tab="头条" key="1">
+                        头条
+                    </TabPane>
+                    <TabPane tab="社会" key="2">
+                        社会
+                    </TabPane>
+                    <TabPane tab="国内" key="3">
+                        国内
+                    </TabPane>
+                    <TabPane tab="国际" key="4">
+                        国际
+                    </TabPane>
+                    <TabPane tab="娱乐" key="5">
+                        娱乐
+                    </TabPane>
+                </Tabs>
                 <MobileFooter/>
             </div>
         );
