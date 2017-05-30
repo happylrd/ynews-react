f49e8d66e501a70b275a0efc6baf6792623681e6
happylrd <happylrd17@gmail.com>
2017-05-30 16:18:11 +0800

    Add login ui and logic for pc and mobile

--- ynews/src/components/MobileHeader.js
+++ ynews/src/components/MobileHeader.js
@@ -28,6 +28,13 @@ class MobileHeader extends Component {
         };
     }
 
+    componentWillMount() {
+        if (localStorage.userId !== '') {
+            this.setState({hasLogin: true});
+            this.setState({userNickName: localStorage.userNickName, userId: localStorage.userId});
+        }
+    }
+
     setModalVisible(value) {
         this.setState({modalVisible: value});
     }
@@ -41,20 +48,42 @@ class MobileHeader extends Component {
         const myFetchOptions = {
             method: 'GET'
         };
-        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName=' +
-            formData.r_username + '&r_password=' + formData.r_password + '&r_confirmPassword=' +
-            formData.r_confirmPassword, myFetchOptions)
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=' + this.state.action +
+            '&username=' + formData.username + '&password=' + formData.password +
+            '&r_userName=' + formData.r_username + '&r_password=' + formData.r_password +
+            '&r_confirmPassword=' + formData.r_confirmPassword, myFetchOptions)
             .then(response => response.json())
             .then(json => {
                 this.setState({userNickName: json.NickUserName, userId: json.UserId});
+                localStorage.userId = json.UserId;
+                localStorage.userNickName = json.NickUserName;
                 message.success("请求成功！");
+
+                //TODO: just mock login success, will be removed later
+                if (this.state.action === 'login') {
+                    this.setState({hasLogin: true});
+                }
             });
 
         this.setModalVisible(false);
     }
 
-    doRegister() {
+    tabsCallback(key) {
+        if (key === '1') {
+            this.setState({action: 'login'});
+        } else if (key === '2') {
+            this.setState({action: 'register'});
+        }
+    }
+
+    handleClick() {
         this.setModalVisible(true);
+    }
+
+    logout() {
+        localStorage.userId = '';
+        localStorage.userNickName = '';
+        this.setState({hasLogin: false});
     }
 
     render() {
@@ -62,9 +91,9 @@ class MobileHeader extends Component {
 
         const userShowItem = this.state.hasLogin
             ?
-            <Icon type="inbox"/>
+            <Icon type="inbox" onClick={this.logout.bind(this)}/>
             :
-            <Icon type="setting" onClick={this.doRegister.bind(this)}/>;
+            <Icon type="setting" onClick={this.handleClick.bind(this)}/>;
 
         return (
             <div id="mobile-header">
@@ -85,7 +114,28 @@ class MobileHeader extends Component {
                     onCancel={() => this.setModalVisible(false)}
                     okText="关闭"
                 >
-                    <Tabs type="card">
+                    <Tabs type="card" onChange={this.tabsCallback.bind(this)}>
+                        <TabPane tab="登录" key="1">
+                            <Form onSubmit={this.handleSubmit.bind(this)}>
+                                <FormItem label="账户">
+                                    {getFieldDecorator('username', {
+                                        rules: [{required: true, message: '请输入您的用户名'}],
+                                    })(
+                                        <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="用户名"/>
+                                    )}
+                                </FormItem>
+                                <FormItem label="密码">
+                                    {getFieldDecorator('password', {
+                                        rules: [{required: true, message: '请输入您的密码'}],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
+                                               placeholder="密码"/>
+                                    )}
+                                </FormItem>
+                                <Button type="primary" htmlType="submit">登录</Button>
+                            </Form>
+                        </TabPane>
+
                         <TabPane tab="注册" key="2">
                             <Form onSubmit={this.handleSubmit.bind(this)}>
                                 <FormItem label="账户">
--- ynews/src/components/PCHeader.js
+++ ynews/src/components/PCHeader.js
@@ -31,6 +31,13 @@ class PCHeader extends Component {
         };
     }
 
+    componentWillMount() {
+        if (localStorage.userId !== '') {
+            this.setState({hasLogin: true});
+            this.setState({userNickName: localStorage.userNickName, userId: localStorage.userId});
+        }
+    }
+
     setModalVisible(value) {
         this.setState({modalVisible: value});
     }
@@ -53,16 +60,38 @@ class PCHeader extends Component {
         const myFetchOptions = {
             method: 'GET'
         };
-        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName=' +
-            formData.r_username + '&r_password=' + formData.r_password + '&r_confirmPassword=' +
-            formData.r_confirmPassword, myFetchOptions)
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=' + this.state.action +
+            '&username=' + formData.username + '&password=' + formData.password +
+            '&r_userName=' + formData.r_username + '&r_password=' + formData.r_password +
+            '&r_confirmPassword=' + formData.r_confirmPassword, myFetchOptions)
             .then(response => response.json())
             .then(json => {
                 this.setState({userNickName: json.NickUserName, userId: json.UserId});
+                localStorage.userId = json.UserId;
+                localStorage.userNickName = json.NickUserName;
                 message.success("请求成功！");
+
+                //TODO: just mock login success, will be removed later
+                if (this.state.action === 'login') {
+                    this.setState({hasLogin: true});
+                }
             });
 
         this.setModalVisible(false);
+    }
+
+    tabsCallback(key) {
+        if (key === '1') {
+            this.setState({action: 'login'});
+        } else if (key === '2') {
+            this.setState({action: 'register'});
+        }
+    }
+
+    logout() {
+        localStorage.userId = '';
+        localStorage.userNickName = '';
+        this.setState({hasLogin: false});
     }
 
     render() {
@@ -77,7 +106,7 @@ class PCHeader extends Component {
                 <Button type="dashed">个人中心</Button>
                 {/*</Link>*/}
                 &nbsp;&nbsp;
-                <Button ghost>退出</Button>
+                <Button onClick={this.logout.bind(this)}>退出</Button>
             </Menu.Item>
             :
             <Menu.Item key="register" class="register">
@@ -146,7 +175,28 @@ class PCHeader extends Component {
                     onCancel={() => this.setModalVisible(false)}
                     okText="关闭"
                 >
-                    <Tabs type="card">
+                    <Tabs type="card" onChange={this.tabsCallback.bind(this)}>
+                        <TabPane tab="登录" key="1">
+                            <Form onSubmit={this.handleSubmit.bind(this)}>
+                                <FormItem label="账户">
+                                    {getFieldDecorator('username', {
+                                        rules: [{required: true, message: '请输入您的用户名'}],
+                                    })(
+                                        <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="用户名"/>
+                                    )}
+                                </FormItem>
+                                <FormItem label="密码">
+                                    {getFieldDecorator('password', {
+                                        rules: [{required: true, message: '请输入您的密码'}],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
+                                               placeholder="密码"/>
+                                    )}
+                                </FormItem>
+                                <Button type="primary" htmlType="submit">登录</Button>
+                            </Form>
+                        </TabPane>
+
                         <TabPane tab="注册" key="2">
                             <Form onSubmit={this.handleSubmit.bind(this)}>
                                 <FormItem label="账户">
